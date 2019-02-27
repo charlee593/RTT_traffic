cf80291035bbb744c96d314d6ec0a9a80e52dacc
carlosl <carlosl@sfu.ca>
2019-02-26 17:04:07 -0800

    add csv

--- ping_rtt.py
+++ ping_rtt.py
@@ -1,6 +1,8 @@
 import subprocess
 import re
 import datetime
+import csv
+import socket
 
 top_site = []
 data = {}
@@ -14,37 +16,35 @@ data["ip"] = []
 data["bytes_of_data"] = []
 data["time"] = []
 data["site"] = []
-
+hostname = socket.gethostname()
 
 f = open("top_canada_site.txt", "r")
 for x in f:
     top_site.append(x.strip('\n'))
 
 
+for site in top_site:
+    p = subprocess.Popen(["ping", "-c", "2", site], stdout=subprocess.PIPE)
 
+    strs = p.communicate()[0].split("\n")
+    str_list = filter(None, strs)
+    raw.append(str_list)
 
+    data["min"].append(str_list[-1][23:].split('/')[0])
+    data["avg"].append(str_list[-1][23:].split('/')[1])
+    data["max"].append(str_list[-1][23:].split('/')[2])
+    data["mdev"].append(str_list[-1][23:].strip('ms').split('/')[3])
+    data["packet loss"].append(re.match(r".*received, (.*)%.*", str_list[-2], re.M|re.I).group(1))
+    data["ip"].append(re.match(r"PING.*\((.*)\) 56\(84\) bytes of data", str_list[0], re.M|re.I).group(1))
+    data["bytes_of_data"].append(re.match(r".*\) ([0-9]*)\(84\) bytes of data", str_list[0], re.M|re.I).group(1))
+    data["time"].append(datetime.datetime.now())
+    data["site"].append(re.match(r"PING (.*) \(.*", str_list[0], re.M|re.I).group(1))
 
+    with open(hostname + '_dict.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in data.items():
+            writer.writerow([key, value])
 
-
-
-p = subprocess.Popen(["ping", "-c", "2", "Wikipedia.org"], stdout=subprocess.PIPE)
-
-strs = p.communicate()[0].split("\n")
-str_list = filter(None, strs)
-raw.append(str_list)
-
-data["min"].append(str_list[-1][23:].split('/')[0])
-data["avg"].append(str_list[-1][23:].split('/')[1])
-data["max"].append(str_list[-1][23:].split('/')[2])
-data["mdev"].append(str_list[-1][23:].strip('ms').split('/')[3])
-data["packet loss"].append(re.match(r".*received, (.*)%.*", str_list[-2], re.M|re.I).group(1))
-data["ip"].append(re.match(r"PING.*\((.*)\) 56\(84\) bytes of data", str_list[0], re.M|re.I).group(1))
-data["bytes_of_data"].append(re.match(r".*\) ([0-9]*)\(84\) bytes of data", str_list[0], re.M|re.I).group(1))
-data["time"].append(datetime.datetime.now())
-data["site"].append(re.match(r"PING (.*) \(.*", str_list[0], re.M|re.I).group(1))
-
-print(str_list)
-print data
-print datetime.datetime.now()
-print(top_site)
+    print hostname
+print(data['time'][0])
 
